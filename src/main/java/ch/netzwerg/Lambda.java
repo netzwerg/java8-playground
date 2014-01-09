515205b56402c9d064577836854177b847d58936
Rahel Lüthy <rahel.luethy@gmail.com>
2014-01-09 11:02:04 +0100

    Add array initializer example

--- src/main/java/ch/netzwerg/Lambda.java
+++ src/main/java/ch/netzwerg/Lambda.java
@@ -8,11 +8,15 @@ import java.util.function.Function;
 @SuppressWarnings("ALL")
 public class Lambda {
 
+    /*-------------------------------------------------------------------------
+      Basic Lambda Syntax
+     -------------------------------------------------------------------------*/
+
     // Generic Function interface
     private static Function<Integer, Integer> times3 = (Integer x) -> {return x * 3;};
 
     // Statement lambda can be replaced with expression lambda
-    private static Function<Integer, Integer> times4 = (Integer x) -> x * 3;
+    private static Function<Integer, Integer> times4 = (Integer x) -> x * 4;
 
     // Argument types may be omitted if compiler can infer them
     private static Function<Integer, Integer> times5 = x -> x * 5;
@@ -25,19 +29,43 @@ public class Lambda {
     // Custom functional interface, again with type inference
     private static Times fortyTwo = x -> x * 42;
 
-    // Method reference
+    // Lambdas in array initializer
+    private static Times[] multipliers = new Times[] {
+        x -> x,
+        x -> x * 2,
+        x -> x * 3
+    };
+
+    /*-------------------------------------------------------------------------
+      Method References
+     -------------------------------------------------------------------------*/
+
+    // Static method reference
     private static Times ninetyNine = Lambda::ninetyNine;
 
     public static int ninetyNine(int x) {
         return x * 99;
     }
 
+    // Instance method reference
+    private static Times fiftyFive = new Lambda()::fiftyFive;
+
+    public int fiftyFive(int x) {
+        return x * 55;
+    }
+
+    /*-------------------------------------------------------------------------
+      Main
+     -------------------------------------------------------------------------*/
+
     public static void main(String[] args) {
         System.out.println("2 * 3 = " + times3.apply(2));
         System.out.println("7 * 4 = " + times4.apply(7));
         System.out.println("6 * 5 = " + times5.apply(6));
         System.out.println("42 * 2 = " + fortyTwo.times(2));
+        System.out.println("3 * 10 = " + multipliers[2].times(10));
         System.out.println("99 * 10 = " + ninetyNine.times(10));
+        System.out.println("55 * 10 = " + fiftyFive.times(10));
     }
 
 }
