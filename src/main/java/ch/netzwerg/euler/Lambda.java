2a411b8a7eed92bf9cc209b4b83b6c2161ded8bd
Rahel Lüthy <rahel.luethy@gmail.com>
2014-01-09 09:25:43 +0100

    Further syntax explorations

--- src/main/java/ch/netzwerg/euler/Lambda.java
+++ src/main/java/ch/netzwerg/euler/Lambda.java
@@ -5,18 +5,30 @@ import java.util.function.Function;
 /**
  * @author rahel.luethy@gmail.com
  */
+@SuppressWarnings("ALL")
 public class Lambda {
 
-    private static Function<Integer, Integer> times3 = (Integer x) -> (x * 3);
+    // Generic Function interface
+    private static Function<Integer, Integer> times3 = (Integer x) -> {return x * 3;};
 
+    // Statement lambda can be replaced with expression lambda
+    private static Function<Integer, Integer> times4 = (Integer x) -> x * 3;
+
+    // Argument types may be omitted if compiler can infer them
+    private static Function<Integer, Integer> times5 = x -> x * 5;
+
+    // Custom functional interface
     interface Times {
         int times(int x);
     }
 
-    private static Times fortyTwo = (int x) -> (x * 42);
+    // Custom functional interface, again with type inference
+    private static Times fortyTwo = x -> x * 42;
 
     public static void main(String[] args) {
         System.out.println("2 * 3 = " + times3.apply(2));
+        System.out.println("7 * 4 = " + times4.apply(7));
+        System.out.println("6 * 5 = " + times5.apply(6));
         System.out.println("42 * 2 = " + fortyTwo.times(2));
     }
 
